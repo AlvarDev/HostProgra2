6e32341cc6a11f044d9b2db1c6ab7bf10e86dbba
AlvarDev <deivid07.21@gmail.com>
2015-06-07 23:44:06 -0500

    json

---
+++ eventdetail.js
@@ -0,0 +1,41 @@
+{
+  "success": "true",
+  "message": "Event found",
+  "event": {
+    "idEvent": "0",
+    "title": "title event",
+    "urlImg": "https://lh5.googleusercontent.com/-T-kdalde_Vo/VW4JwTU4sEI/AAAAAAAAIDw/T_LB0V7-z-A/w613-h605-no/karla.jpg",
+    "description": "text for description",
+    "address": "Av. La Fontana",
+    "date": "Del 16 al 30 de Noviembre",
+    "latlng": "-12.10363400,-76.97180720",
+    "tickets": [
+      {
+        "sector": "vip",
+        "price": "25"
+      },
+      {
+        "sector": "general",
+        "price": "15"
+      }
+    ],
+    "comments": [
+      {
+        "id": "0",
+        "title": "Excelente",
+        "message": "Muy bueno",
+        "urlImg": "https://lh5.googleusercontent.com/-T-kdalde_Vo/VW4JwTU4sEI/AAAAAAAAIDw/T_LB0V7-z-A/w613-h605-no/karla.jpg",
+        "timeAgo": "15m",
+        "favorite": true
+      },
+      {
+        "id": "1",
+        "title": "Excelente",
+        "message": "Muy bueno",
+        "urlImg": "https://lh5.googleusercontent.com/-T-kdalde_Vo/VW4JwTU4sEI/AAAAAAAAIDw/T_LB0V7-z-A/w613-h605-no/karla.jpg",
+        "timeAgo": "25m",
+        "favorite": false
+      }
+    ]
+  }
+}
---
+++ events.js
@@ -0,0 +1,18 @@
+{
+  "success": true,
+  "message": "Search success",
+  "events": [
+    {
+      "id": "0",
+      "title": "title of the event",
+      "description": "text for description",
+      "urlImg": "https://lh5.googleusercontent.com/-T-kdalde_Vo/VW4JwTU4sEI/AAAAAAAAIDw/T_LB0V7-z-A/w613-h605-no/karla.jpg"
+    },
+    {
+      "id": "1",
+      "title": "title of the event",
+      "description": "text for description",
+      "urlImg": "https://lh5.googleusercontent.com/-T-kdalde_Vo/VW4JwTU4sEI/AAAAAAAAIDw/T_LB0V7-z-A/w613-h605-no/karla.jpg"
+    }
+  ]
+}
