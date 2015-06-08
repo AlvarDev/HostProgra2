1d36b89faf7e53a3d7ffb58f8331d356150f277a
AlvarDev <deivid07.21@gmail.com>
2015-06-07 20:16:34 -0500

    done

---
+++ login.js
@@ -0,0 +1,21 @@
+{
+​ success": true,
+"
+"message": "Success Login",
+​
+"user": {
+​
+"id": "0",
+"name": "AlvarDev",
+​
+"email": "alvardev@google.com",
+​
+"urlImg":
+​
+" ​
+https://lh5.googleusercontent.com/-T-kdalde_Vo/VW4JwTU4sEI/AAAAA
+AAAIDw/T_LB0V7-z-A/w613-h605-no/karla.jpg ​
+"
+}
+​
+}
