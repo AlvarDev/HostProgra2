20a814c971e22f019427ab65c40664368be26f59
AlvarDev <deivid07.21@gmail.com>
2015-06-07 20:19:15 -0500

    update

--- login.js
+++ login.js
@@ -1,21 +1,11 @@
 {
-​ success": true,
-"
-"message": "Success Login",
-​
-"user": {
-​
-"id": "0",
-"name": "AlvarDev",
-​
-"email": "alvardev@google.com",
-​
-"urlImg":
-​
-" ​
-https://lh5.googleusercontent.com/-T-kdalde_Vo/VW4JwTU4sEI/AAAAA
-AAAIDw/T_LB0V7-z-A/w613-h605-no/karla.jpg ​
-"
-}
-​
+  "success": true,
+  "message": "Success Login",
+  "user": {
+    "idusuario": "2",
+    "nombre": "Alvaro",
+    "email": "alvardev@google.com",
+    "password": "123claudia07",
+    "urlImg": "https://lh5.googleusercontent.com/-T-kdalde_Vo/VW4JwTU4sEI/AAAAAAAAIDw/T_LB0V7-z-A/w613-h605-no/karla.jpg"
+  }
 }
