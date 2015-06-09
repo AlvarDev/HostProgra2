2f1ccc496c9129dbc0ca9b8c7c34473c0d8ec893
AlvarDev <deivid07.21@gmail.com>
2015-06-09 00:39:48 -0500

    postal code

---
+++ postalcode.js
@@ -0,0 +1,27 @@
+{
+  "success": true,
+  "message": "Events found",
+  "events": [
+    {
+      "latlng": "-12.2174109,-76.9377649",
+      "title": "capilla barrio",
+      "type": "1"
+    },
+    {
+      "latlng": "-12.2176825,-76.9730211",
+      "title": "capilla la cruz",
+      "type": "2"
+    },
+    {
+      "latlng": "-12.2195643,-76.9406763",
+      "title": "capilla Guadalupe",
+      "type": "2"
+    },
+    {
+      "latlng": "-12.218774,-76.9362556",
+      "title": "colegio pachacutec",
+      "type": "3"
+    }
+  ]
+}
+
