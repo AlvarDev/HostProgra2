9daadd2064911f9c9df91ebe587b88c86ebb9ed2
AlvarDev <ahuanca@usmpvirtual.edu.pe>
2015-07-21 12:38:36 -0500

    notas2

--- notas.js
+++ notas.js
@@ -1,264 +1,254 @@
 {
   "notas": [
     {
-      "nota": {
-        "nombreCurso": "DISEÑO E IMPLEMENTACIÓN DE SISTEMAS",
-        "promedioFinal": "14",
-        "notas": [
-          {
-            "desc": "Exámen Final",
-            "abreviatura": "EF",
-            "nota": "13"
-          },
-          {
-            "desc": "Exámen Parcial",
-            "abreviatura": "EP",
-            "nota": "11"
-          },
-          {
-            "desc": "Informe 1",
-            "abreviatura": "I1",
-            "nota": "17"
-          },
-          {
-            "desc": "Prueba de Entrada",
-            "abreviatura": "P0",
-            "nota": "11"
-          },
-          {
-            "desc": "Practica Calificada 1",
-            "abreviatura": "P1",
-            "nota": "16"
-          },
-          {
-            "desc": "Practica Calificada 2",
-            "abreviatura": "P2",
-            "nota": "13"
-          },
-          {
-            "desc": "Promedio de Evaluaciones",
-            "abreviatura": "PE",
-            "nota": "14.80"
-          },
-          {
-            "desc": "Promedio de Laboratorio",
-            "abreviatura": "PL",
-            "nota": "15.75"
-          }
-        ],
-        "formulas": [
-          "PE=(P0+(P1+P2)/2+I1+PL*2)/5",
-          "PF=(EP+PE+EF)/3"
-        ]
-      }
+      "nombreCurso": "DISEÑO E IMPLEMENTACIÓN DE SISTEMAS",
+      "promedioFinal": "14",
+      "notas": [
+        {
+          "desc": "Exámen Final",
+          "abreviatura": "EF",
+          "nota": "13"
+        },
+        {
+          "desc": "Exámen Parcial",
+          "abreviatura": "EP",
+          "nota": "11"
+        },
+        {
+          "desc": "Informe 1",
+          "abreviatura": "I1",
+          "nota": "17"
+        },
+        {
+          "desc": "Prueba de Entrada",
+          "abreviatura": "P0",
+          "nota": "11"
+        },
+        {
+          "desc": "Practica Calificada 1",
+          "abreviatura": "P1",
+          "nota": "16"
+        },
+        {
+          "desc": "Practica Calificada 2",
+          "abreviatura": "P2",
+          "nota": "13"
+        },
+        {
+          "desc": "Promedio de Evaluaciones",
+          "abreviatura": "PE",
+          "nota": "14.80"
+        },
+        {
+          "desc": "Promedio de Laboratorio",
+          "abreviatura": "PL",
+          "nota": "15.75"
+        }
+      ],
+      "formulas": [
+        "PE=(P0+(P1+P2)/2+I1+PL*2)/5",
+        "PF=(EP+PE+EF)/3"
+      ]
     },
     {
-      "nota": {
-        "nombreCurso": "LABORATORIO DE DISEÑO E IMPLEMENTACIÓN DE SISTEMAS",
-        "promedioFinal": "14",
-        "notas": [
-          {
-            "desc": "Exámen Final",
-            "abreviatura": "EF",
-            "nota": "13"
-          },
-          {
-            "desc": "Exámen Parcial",
-            "abreviatura": "EP",
-            "nota": "11"
-          },
-          {
-            "desc": "Informe 1",
-            "abreviatura": "I1",
-            "nota": "17"
-          },
-          {
-            "desc": "Prueba de Entrada",
-            "abreviatura": "P0",
-            "nota": "11"
-          },
-          {
-            "desc": "Practica Calificada 1",
-            "abreviatura": "P1",
-            "nota": "16"
-          },
-          {
-            "desc": "Practica Calificada 2",
-            "abreviatura": "P2",
-            "nota": "13"
-          },
-          {
-            "desc": "Promedio de Evaluaciones",
-            "abreviatura": "PE",
-            "nota": "14.80"
-          },
-          {
-            "desc": "Promedio de Laboratorio",
-            "abreviatura": "PL",
-            "nota": "15.75"
-          }
-        ],
-        "formulas": [
-          "PE=(P0+(P1+P2)/2+I1+PL*2)/5",
-          "PF=(EP+PE+EF)/3"
-        ]
-      }
+      "nombreCurso": "LABORATORIO DE DISEÑO E IMPLEMENTACIÓN DE SISTEMAS",
+      "promedioFinal": "14",
+      "notas": [
+        {
+          "desc": "Exámen Final",
+          "abreviatura": "EF",
+          "nota": "13"
+        },
+        {
+          "desc": "Exámen Parcial",
+          "abreviatura": "EP",
+          "nota": "11"
+        },
+        {
+          "desc": "Informe 1",
+          "abreviatura": "I1",
+          "nota": "17"
+        },
+        {
+          "desc": "Prueba de Entrada",
+          "abreviatura": "P0",
+          "nota": "11"
+        },
+        {
+          "desc": "Practica Calificada 1",
+          "abreviatura": "P1",
+          "nota": "16"
+        },
+        {
+          "desc": "Practica Calificada 2",
+          "abreviatura": "P2",
+          "nota": "13"
+        },
+        {
+          "desc": "Promedio de Evaluaciones",
+          "abreviatura": "PE",
+          "nota": "14.80"
+        },
+        {
+          "desc": "Promedio de Laboratorio",
+          "abreviatura": "PL",
+          "nota": "15.75"
+        }
+      ],
+      "formulas": [
+        "PE=(P0+(P1+P2)/2+I1+PL*2)/5",
+        "PF=(EP+PE+EF)/3"
+      ]
     },
     {
-      "nota": {
-        "nombreCurso": "INGLES I",
-        "promedioFinal": "14",
-        "notas": [
-          {
-            "desc": "Exámen Final",
-            "abreviatura": "EF",
-            "nota": "13"
-          },
-          {
-            "desc": "Exámen Parcial",
-            "abreviatura": "EP",
-            "nota": "11"
-          },
-          {
-            "desc": "Informe 1",
-            "abreviatura": "I1",
-            "nota": "17"
-          },
-          {
-            "desc": "Prueba de Entrada",
-            "abreviatura": "P0",
-            "nota": "11"
-          },
-          {
-            "desc": "Practica Calificada 1",
-            "abreviatura": "P1",
-            "nota": "16"
-          },
-          {
-            "desc": "Practica Calificada 2",
-            "abreviatura": "P2",
-            "nota": "13"
-          },
-          {
-            "desc": "Promedio de Evaluaciones",
-            "abreviatura": "PE",
-            "nota": "14.80"
-          },
-          {
-            "desc": "Promedio de Laboratorio",
-            "abreviatura": "PL",
-            "nota": "15.75"
-          }
-        ],
-        "formulas": [
-          "PE=(P0+(P1+P2)/2+I1+PL*2)/5",
-          "PF=(EP+PE+EF)/3"
-        ]
-      }
+      "nombreCurso": "INGLES I",
+      "promedioFinal": "14",
+      "notas": [
+        {
+          "desc": "Exámen Final",
+          "abreviatura": "EF",
+          "nota": "13"
+        },
+        {
+          "desc": "Exámen Parcial",
+          "abreviatura": "EP",
+          "nota": "11"
+        },
+        {
+          "desc": "Informe 1",
+          "abreviatura": "I1",
+          "nota": "17"
+        },
+        {
+          "desc": "Prueba de Entrada",
+          "abreviatura": "P0",
+          "nota": "11"
+        },
+        {
+          "desc": "Practica Calificada 1",
+          "abreviatura": "P1",
+          "nota": "16"
+        },
+        {
+          "desc": "Practica Calificada 2",
+          "abreviatura": "P2",
+          "nota": "13"
+        },
+        {
+          "desc": "Promedio de Evaluaciones",
+          "abreviatura": "PE",
+          "nota": "14.80"
+        },
+        {
+          "desc": "Promedio de Laboratorio",
+          "abreviatura": "PL",
+          "nota": "15.75"
+        }
+      ],
+      "formulas": [
+        "PE=(P0+(P1+P2)/2+I1+PL*2)/5",
+        "PF=(EP+PE+EF)/3"
+      ]
     },
     {
-      "nota": {
-        "nombreCurso": "PROGRAMACIÓN II",
-        "promedioFinal": "14",
-        "notas": [
-          {
-            "desc": "Exámen Final",
-            "abreviatura": "EF",
-            "nota": "13"
-          },
-          {
-            "desc": "Exámen Parcial",
-            "abreviatura": "EP",
-            "nota": "11"
-          },
-          {
-            "desc": "Informe 1",
-            "abreviatura": "I1",
-            "nota": "17"
-          },
-          {
-            "desc": "Prueba de Entrada",
-            "abreviatura": "P0",
-            "nota": "11"
-          },
-          {
-            "desc": "Practica Calificada 1",
-            "abreviatura": "P1",
-            "nota": "16"
-          },
-          {
-            "desc": "Practica Calificada 2",
-            "abreviatura": "P2",
-            "nota": "13"
-          },
-          {
-            "desc": "Promedio de Evaluaciones",
-            "abreviatura": "PE",
-            "nota": "14.80"
-          },
-          {
-            "desc": "Promedio de Laboratorio",
-            "abreviatura": "PL",
-            "nota": "15.75"
-          }
-        ],
-        "formulas": [
-          "PE=(P0+(P1+P2)/2+I1+PL*2)/5",
-          "PF=(EP+PE+EF)/3"
-        ]
-      }
+      "nombreCurso": "PROGRAMACIÓN II",
+      "promedioFinal": "14",
+      "notas": [
+        {
+          "desc": "Exámen Final",
+          "abreviatura": "EF",
+          "nota": "13"
+        },
+        {
+          "desc": "Exámen Parcial",
+          "abreviatura": "EP",
+          "nota": "11"
+        },
+        {
+          "desc": "Informe 1",
+          "abreviatura": "I1",
+          "nota": "17"
+        },
+        {
+          "desc": "Prueba de Entrada",
+          "abreviatura": "P0",
+          "nota": "11"
+        },
+        {
+          "desc": "Practica Calificada 1",
+          "abreviatura": "P1",
+          "nota": "16"
+        },
+        {
+          "desc": "Practica Calificada 2",
+          "abreviatura": "P2",
+          "nota": "13"
+        },
+        {
+          "desc": "Promedio de Evaluaciones",
+          "abreviatura": "PE",
+          "nota": "14.80"
+        },
+        {
+          "desc": "Promedio de Laboratorio",
+          "abreviatura": "PL",
+          "nota": "15.75"
+        }
+      ],
+      "formulas": [
+        "PE=(P0+(P1+P2)/2+I1+PL*2)/5",
+        "PF=(EP+PE+EF)/3"
+      ]
     },
     {
-      "nota": {
-        "nombreCurso": "PROYECTOS I",
-        "promedioFinal": "14",
-        "notas": [
-          {
-            "desc": "Exámen Final",
-            "abreviatura": "EF",
-            "nota": "13"
-          },
-          {
-            "desc": "Exámen Parcial",
-            "abreviatura": "EP",
-            "nota": "11"
-          },
-          {
-            "desc": "Informe 1",
-            "abreviatura": "I1",
-            "nota": "17"
-          },
-          {
-            "desc": "Prueba de Entrada",
-            "abreviatura": "P0",
-            "nota": "11"
-          },
-          {
-            "desc": "Practica Calificada 1",
-            "abreviatura": "P1",
-            "nota": "16"
-          },
-          {
-            "desc": "Practica Calificada 2",
-            "abreviatura": "P2",
-            "nota": "13"
-          },
-          {
-            "desc": "Promedio de Evaluaciones",
-            "abreviatura": "PE",
-            "nota": "14.80"
-          },
-          {
-            "desc": "Promedio de Laboratorio",
-            "abreviatura": "PL",
-            "nota": "15.75"
-          }
-        ],
-        "formulas": [
-          "PE=(P0+(P1+P2)/2+I1+PL*2)/5",
-          "PF=(EP+PE+EF)/3"
-        ]
-      }
+      "nombreCurso": "PROYECTOS I",
+      "promedioFinal": "14",
+      "notas": [
+        {
+          "desc": "Exámen Final",
+          "abreviatura": "EF",
+          "nota": "13"
+        },
+        {
+          "desc": "Exámen Parcial",
+          "abreviatura": "EP",
+          "nota": "11"
+        },
+        {
+          "desc": "Informe 1",
+          "abreviatura": "I1",
+          "nota": "17"
+        },
+        {
+          "desc": "Prueba de Entrada",
+          "abreviatura": "P0",
+          "nota": "11"
+        },
+        {
+          "desc": "Practica Calificada 1",
+          "abreviatura": "P1",
+          "nota": "16"
+        },
+        {
+          "desc": "Practica Calificada 2",
+          "abreviatura": "P2",
+          "nota": "13"
+        },
+        {
+          "desc": "Promedio de Evaluaciones",
+          "abreviatura": "PE",
+          "nota": "14.80"
+        },
+        {
+          "desc": "Promedio de Laboratorio",
+          "abreviatura": "PL",
+          "nota": "15.75"
+        }
+      ],
+      "formulas": [
+        "PE=(P0+(P1+P2)/2+I1+PL*2)/5",
+        "PF=(EP+PE+EF)/3"
+      ]
     }
   ]
 }
