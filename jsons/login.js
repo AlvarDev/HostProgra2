fbc57a566fdb3fa161594a3251ee016288af63b2
AlvarDev <ahuanca@usmpvirtual.edu.pe>
2015-10-21 12:36:52 -0500

    dota

---
+++ jsons/login.js
@@ -0,0 +1,8 @@
+{
+  "success": true,
+  "alumno": {
+    "nombreCompleto": "Alvaro David",
+    "correo": "deivid07.21@alvardev.com",
+    "carrera": "Game Player for DotA"
+  }
+}
---
+++ jsons/notas.js
@@ -0,0 +1,33 @@
+{
+  "success": true,
+  "ulrimoMesMatriculado": "Enero",
+  "notas": [
+    {
+      "nombre": "Support Heroes",
+      "ciclo": 4,
+      "nota": 19.5,
+      "creditos": 25,
+      "mes": "Febrero",
+      "aprobado": false,
+      "cursando": true
+    },
+    {
+      "nombre": "Carrier Heroes",
+      "ciclo": 5,
+      "nota": 16.5,
+      "creditos": 25,
+      "mes": "Febrero",
+      "aprobado": true,
+      "cursando": false
+    },
+    {
+      "nombre": "Curier Heroes",
+      "ciclo": 6,
+      "nota": 11.5,
+      "creditos": 25,
+      "mes": "Febrero",
+      "aprobado": false,
+      "cursando": false
+    }
+  ]
+}
