041886d04da1c03fc1654d9d3bfd661d31d7c802
AlvarDev <ahuanca@usmpvirtual.edu.pe>
2015-12-07 01:39:55 -0500

    adding pav

---
+++ pav/miprograso.js
@@ -0,0 +1,293 @@
+{
+  "success": true,
+  "cantCiclos": 7,
+  "ciclos": [
+    {
+      "numCursos": 7,
+      "numCursosAprob": 7,
+      "cursos": [
+        {
+          "nombre": "Curso de prueba 0",
+          "creditos": "4",
+          "aprobado": true
+        },
+        {
+          "nombre": "Curso de prueba 1",
+          "creditos": "4",
+          "aprobado": true
+        },
+        {
+          "nombre": "Curso de prueba 2",
+          "creditos": "4",
+          "aprobado": true
+        },
+        {
+          "nombre": "Curso de prueba 3",
+          "creditos": "4",
+          "aprobado": true
+        },
+        {
+          "nombre": "Curso de prueba 4",
+          "creditos": "4",
+          "aprobado": true
+        },
+        {
+          "nombre": "Curso de prueba 5",
+          "creditos": "4",
+          "aprobado": true
+        },
+        {
+          "nombre": "Curso de prueba 6",
+          "creditos": "4",
+          "aprobado": true
+        }
+      ]
+    },
+    {
+      "numCursos": 7,
+      "numCursosAprob": 7,
+      "cursos": [
+        {
+          "nombre": "Curso de prueba 0",
+          "creditos": "4",
+          "aprobado": true
+        },
+        {
+          "nombre": "Curso de prueba 1",
+          "creditos": "4",
+          "aprobado": true
+        },
+        {
+          "nombre": "Curso de prueba 2",
+          "creditos": "4",
+          "aprobado": true
+        },
+        {
+          "nombre": "Curso de prueba 3",
+          "creditos": "4",
+          "aprobado": true
+        },
+        {
+          "nombre": "Curso de prueba 4",
+          "creditos": "4",
+          "aprobado": true
+        },
+        {
+          "nombre": "Curso de prueba 5",
+          "creditos": "4",
+          "aprobado": true
+        },
+        {
+          "nombre": "Curso de prueba 6",
+          "creditos": "4",
+          "aprobado": true
+        }
+      ]
+    },
+    {
+      "numCursos": 7,
+      "numCursosAprob": 7,
+      "cursos": [
+        {
+          "nombre": "Curso de prueba 0",
+          "creditos": "4",
+          "aprobado": true
+        },
+        {
+          "nombre": "Curso de prueba 1",
+          "creditos": "4",
+          "aprobado": true
+        },
+        {
+          "nombre": "Curso de prueba 2",
+          "creditos": "4",
+          "aprobado": true
+        },
+        {
+          "nombre": "Curso de prueba 3",
+          "creditos": "4",
+          "aprobado": true
+        },
+        {
+          "nombre": "Curso de prueba 4",
+          "creditos": "4",
+          "aprobado": true
+        },
+        {
+          "nombre": "Curso de prueba 5",
+          "creditos": "4",
+          "aprobado": true
+        },
+        {
+          "nombre": "Curso de prueba 6",
+          "creditos": "4",
+          "aprobado": true
+        }
+      ]
+    },
+    {
+      "numCursos": 7,
+      "numCursosAprob": 7,
+      "cursos": [
+        {
+          "nombre": "Curso de prueba 0",
+          "creditos": "4",
+          "aprobado": true
+        },
+        {
+          "nombre": "Curso de prueba 1",
+          "creditos": "4",
+          "aprobado": true
+        },
+        {
+          "nombre": "Curso de prueba 2",
+          "creditos": "4",
+          "aprobado": true
+        },
+        {
+          "nombre": "Curso de prueba 3",
+          "creditos": "4",
+          "aprobado": true
+        },
+        {
+          "nombre": "Curso de prueba 4",
+          "creditos": "4",
+          "aprobado": true
+        },
+        {
+          "nombre": "Curso de prueba 5",
+          "creditos": "4",
+          "aprobado": true
+        },
+        {
+          "nombre": "Curso de prueba 6",
+          "creditos": "4",
+          "aprobado": true
+        }
+      ]
+    },
+    {
+      "numCursos": 7,
+      "numCursosAprob": 4,
+      "cursos": [
+        {
+          "nombre": "Curso de prueba 0",
+          "creditos": "4",
+          "aprobado": true
+        },
+        {
+          "nombre": "Curso de prueba 1",
+          "creditos": "4",
+          "aprobado": true
+        },
+        {
+          "nombre": "Curso de prueba 2",
+          "creditos": "4",
+          "aprobado": true
+        },
+        {
+          "nombre": "Curso de prueba 3",
+          "creditos": "4",
+          "aprobado": true
+        },
+        {
+          "nombre": "Curso de prueba 4",
+          "creditos": "4",
+          "aprobado": false
+        },
+        {
+          "nombre": "Curso de prueba 5",
+          "creditos": "4",
+          "aprobado": false
+        },
+        {
+          "nombre": "Curso de prueba 6",
+          "creditos": "4",
+          "aprobado": false
+        }
+      ]
+    },
+    {
+      "numCursos": 7,
+      "numCursosAprob": 0,
+      "cursos": [
+        {
+          "nombre": "Curso de prueba 0",
+          "creditos": "4",
+          "aprobado": false
+        },
+        {
+          "nombre": "Curso de prueba 1",
+          "creditos": "4",
+          "aprobado": false
+        },
+        {
+          "nombre": "Curso de prueba 2",
+          "creditos": "4",
+          "aprobado": false
+        },
+        {
+          "nombre": "Curso de prueba 3",
+          "creditos": "4",
+          "aprobado": false
+        },
+        {
+          "nombre": "Curso de prueba 4",
+          "creditos": "4",
+          "aprobado": false
+        },
+        {
+          "nombre": "Curso de prueba 5",
+          "creditos": "4",
+          "aprobado": false
+        },
+        {
+          "nombre": "Curso de prueba 6",
+          "creditos": "4",
+          "aprobado": false
+        }
+      ]
+    },
+    {
+      "numCursos": 7,
+      "numCursosAprob": 2,
+      "cursos": [
+        {
+          "nombre": "Curso de prueba 0",
+          "creditos": "4",
+          "aprobado": true
+        },
+        {
+          "nombre": "Curso de prueba 1",
+          "creditos": "4",
+          "aprobado": true
+        },
+        {
+          "nombre": "Curso de prueba 2",
+          "creditos": "4",
+          "aprobado": false
+        },
+        {
+          "nombre": "Curso de prueba 3",
+          "creditos": "4",
+          "aprobado": false
+        },
+        {
+          "nombre": "Curso de prueba 4",
+          "creditos": "4",
+          "aprobado": false
+        },
+        {
+          "nombre": "Curso de prueba 5",
+          "creditos": "4",
+          "aprobado": false
+        },
+        {
+          "nombre": "Curso de prueba 6",
+          "creditos": "4",
+          "aprobado": false
+        }
+      ]
+    }
+  ]
+}
