55b57f3ca4e0cbb89f47c1d1288237cc6413a7ff
Alvaro David <deivid07.21@gmail.com>
2015-12-13 19:30:43 -0500

    new json for NotasPav

--- pav/notas.js
+++ pav/notas.js
@@ -1,52 +1,54 @@
 {
-  "succes": true,
-  "condicion": "Regular",
-  "ultMes": "Marzo 2015",
-  "cursosAprob": 7,
-  "creditosAprob": 23,
-  "promGeneral": 15.5,
-  "notas": [
-    {
-      "nombreCurso": "Dota 2",
-      "ciclo": 7,
-      "creditos": 7,
-      "nota": 21
-    },
-    {
-      "nombreCurso": "Dota 2",
-      "ciclo": 7,
-      "creditos": 7,
-      "nota": 21
-    },
-    {
-      "nombreCurso": "Dota 2",
-      "ciclo": 7,
-      "creditos": 7,
-      "nota": 21
-    },
-    {
-      "nombreCurso": "Dota 2",
-      "ciclo": 7,
-      "creditos": 7,
-      "nota": 21
-    },
-    {
-      "nombreCurso": "Dota 2",
-      "ciclo": 7,
-      "creditos": 7,
-      "nota": 21
-    },
-    {
-      "nombreCurso": "Dota 2",
-      "ciclo": 7,
-      "creditos": 7,
-      "nota": 21
-    },
-    {
-      "nombreCurso": "Dota 2",
-      "ciclo": 7,
-      "creditos": 7,
-      "nota": 21
-    }
-  ]
+  "success": true,
+  "info": {
+    "condicion": "Regular",
+    "ultMes": "Marzo 2015",
+    "cursosAprob": 7,
+    "creditosAprob": 23,
+    "promGeneral": 15.75,
+    "notas": [
+      {
+        "nombreCurso": "Dota 2",
+        "ciclo": 7,
+        "creditos": 7,
+        "nota": 21
+      },
+      {
+        "nombreCurso": "Dota 2",
+        "ciclo": 7,
+        "creditos": 7,
+        "nota": 21
+      },
+      {
+        "nombreCurso": "Dota 2",
+        "ciclo": 7,
+        "creditos": 7,
+        "nota": 21
+      },
+      {
+        "nombreCurso": "Dota 2",
+        "ciclo": 7,
+        "creditos": 7,
+        "nota": 21
+      },
+      {
+        "nombreCurso": "Dota 2",
+        "ciclo": 7,
+        "creditos": 7,
+        "nota": 21
+      },
+      {
+        "nombreCurso": "Dota 2",
+        "ciclo": 7,
+        "creditos": 7,
+        "nota": 21
+      },
+      {
+        "nombreCurso": "Dota 2",
+        "ciclo": 7,
+        "creditos": 7,
+        "nota": 21
+      }
+    ]
+  }
 }
