a95e5381e8968fd772fd5631039b245b8c7a7404
Alvaro David <deivid07.21@gmail.com>
2015-12-13 22:14:48 -0500

    Add Nombre

--- pav/login.js
+++ pav/login.js
@@ -3,8 +3,9 @@
   "user": {
     "codigoEstudiante": 123456789,
     "usuario": "v123456789",
-    "apePaterno": "Alberto",
-    "apeMaterno": "Canturin",
+    "nombre": "CLAUDIA PATRICIA", 
+    "apePaterno": "Cedeño",
+    "apeMaterno": "Caceres",
     "correo": "acanturin@usmpvirtual.edu.pe",
     "programa": "dota 2"
   }
