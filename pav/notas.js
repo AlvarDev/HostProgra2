82552fb3bcc114c4bd1ae9fa0d2d164b3dbd1fff
Alvaro David <deivid07.21@gmail.com>
2015-12-13 22:13:55 -0500

    Change nota to String

--- pav/notas.js
+++ pav/notas.js
@@ -5,49 +5,49 @@
     "ultMes": "Marzo 2015",
     "cursosAprob": 7,
     "creditosAprob": 23,
-    "promGeneral": 15.75,
+    "promGeneral": "15.75",
     "notas": [
       {
         "nombreCurso": "Dota 2",
         "ciclo": 7,
         "creditos": 7,
-        "nota": 21
+        "nota": "21"
       },
       {
         "nombreCurso": "Dota 2",
         "ciclo": 7,
         "creditos": 7,
-        "nota": 21
+        "nota": "21"
       },
       {
         "nombreCurso": "Dota 2",
         "ciclo": 7,
         "creditos": 7,
-        "nota": 21
+        "nota": "21"
       },
       {
         "nombreCurso": "Dota 2",
         "ciclo": 7,
         "creditos": 7,
-        "nota": 21
+        "nota": "21"
       },
       {
         "nombreCurso": "Dota 2",
         "ciclo": 7,
         "creditos": 7,
-        "nota": 21
+        "nota": "21"
       },
       {
         "nombreCurso": "Dota 2",
         "ciclo": 7,
         "creditos": 7,
-        "nota": 21
+        "nota": "21"
       },
       {
         "nombreCurso": "Dota 2",
         "ciclo": 7,
         "creditos": 7,
-        "nota": 21
+        "nota": "21"
       }
     ]
   }
